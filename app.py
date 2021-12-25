51f4c720471ef252b2f765bf3b31d0154b6f860e
Prashant Kalyani <kalyaniprashant7@gmail.com>
2021-12-25 01:13:48 -0500

    16 commit wkhtmltopdf-pack-ng

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ def _get_pdfkit_config():
      if platform.system() == "Windows":
         return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
      else:
-        os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable) 
-        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], 
-            stdout=subprocess.PIPE).communicate()[0].strip()
+        # os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable) 
+        # WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], 
+        #     stdout=subprocess.PIPE).communicate()[0].strip()
+        # return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/app/bin/wkhtmltopdf')],
+        stdout=subprocess.PIPE).communicate()[0].strip()
         return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
     
 
